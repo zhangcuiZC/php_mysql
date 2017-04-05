bc02ea76250efc096497964a7de5f074df95e325
ZhangCui <zczhangcui@foxmail.com>
2017-04-05 14:29:55 +0800

    update

--- removeemail.php
+++ removeemail.php
@@ -1,14 +1,31 @@
+<p>Please select the email addresses to delete from the email list and click Remove.</p>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	
 <?php
 	$dbc = mysqli_connect('localhost', 'root', '', 'elvis_store')
 		or die('Error connecting to server.');
 
-	$email = $_POST['email'];
+	if (isset($_POST['submit'])) {
+		foreach ($_POST['todelete'] as $delete_id) {
+			$query = "delete from email_list where id=$delete_id";
+			mysqli_query($dbc, $query) or die('Error querying database.');
+		}
+		echo 'Customer(s) removed.<br><br>';
+	}
 
-	$query = "delete from email_list where email='$email'";
+	$query = "select * from email_list";
+	$result = mysqli_query($dbc, $query);
 
-	mysqli_query($dbc, $query) or die('Error querying database');
-
-	echo "Success to remove '$email'";
+	while ($row = mysqli_fetch_array($result)) {
+		echo '<label><input type="checkbox" value="' . $row['id'] . '" name="todelete[]">';
+		echo $row['first_name'];
+		echo ' ' . $row['last_name'];
+		echo ' - ' . $row['email'] . '</label>';
+		echo '<br>';
+	}
 
 	mysqli_close($dbc);
 ?>
+	<input type="submit" name="submit" value="Remove">
+</form>
